a2c8ac298943a6323724719a1760f6608fda4256
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-08-30 20:22:53 +0300

    project initialized

---
+++ app/src/main/java/com/okan/mvi_repository_pattern_with_hilt/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.okan.mvi_repository_pattern_with_hilt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
