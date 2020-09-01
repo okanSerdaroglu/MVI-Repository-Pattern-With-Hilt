e492c8c3b7b87754156e8f608deb9fb15d4c778d
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-09-01 11:01:02 +0300

    MainViewModel State Events

--- app/src/main/java/com/okan/mvi_repository_pattern_with_hilt/ui/MainViewModel.kt
+++ app/src/main/java/com/okan/mvi_repository_pattern_with_hilt/ui/MainViewModel.kt
@@ -2,16 +2,54 @@ package com.okan.mvi_repository_pattern_with_hilt.ui
 
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.okan.mvi_repository_pattern_with_hilt.model.Blog
 import com.okan.mvi_repository_pattern_with_hilt.repository.MainRepository
+import com.okan.mvi_repository_pattern_with_hilt.util.DataState
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 
+@ExperimentalCoroutinesApi
 class MainViewModel
 @ViewModelInject
 constructor(
     private val mainRepository: MainRepository,
     @Assisted private val savedStateHandle: MainRepository
-) : ViewModel()
-{
+) : ViewModel() {
+
+    private val _dataState: MutableLiveData<DataState<List<Blog>>> = MutableLiveData()
+
+    val dataState: LiveData<DataState<List<Blog>>>
+        get() = _dataState
+
+    fun setStateEvent(mainStateEvent: MainStateEvent) {
+        viewModelScope.launch {
+            when (mainStateEvent) {
+                is MainStateEvent.GetBlogEvents -> {
+                    mainRepository.getBlog()
+                        .onEach { dataState ->
+                            _dataState.value = dataState
+                        }.launchIn(viewModelScope)
+                }
+
+                is MainStateEvent.None -> {
+                }
+            }
+        }
+    }
+
+}
+
+sealed class MainStateEvent {
+
+    object GetBlogEvents : MainStateEvent()
+
+    object None : MainStateEvent()
 
 }
